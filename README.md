930bfd60e6e685ad8664c47bbf8773f903c7c773
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-03-24 13:35:57 +0000

    added testing infrastructure, birthdays test and workflow

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 <!-- badges: start -->
 [![R-CMD-check](https://github.com/joshwlambert/ExtendDataFrames/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/joshwlambert/ExtendDataFrames/actions/workflows/R-CMD-check.yaml)
+[![Codecov test coverage](https://codecov.io/gh/joshwlambert/ExtendDataFrames/branch/main/graph/badge.svg)](https://app.codecov.io/gh/joshwlambert/ExtendDataFrames?branch=main)
 <!-- badges: end -->
 
 The goal of ExtendDataFrames is to demonstrate how to extend the data frames
---
+++ tests/testthat/test-birthdays.R
@@ -0,0 +1,30 @@
+test_that("birthdays constructors works as expected", {
+  df <- data.frame(
+     name = c("kevin", "stacey"),
+     birthday = c(as.Date("2001-01-01"), as.Date("2002-01-02"))
+   )
+   birthdays <- birthdays(df)
+
+   expect_s3_class(birthdays, "birthdays")
+   expect_s3_class(birthdays, "data.frame")
+})
+
+test_that("birthdays constructors fails as expected", {
+  df <- data.frame(
+    name = c("kevin", "stacey")
+  )
+ expect_error(
+   birthdays(df),
+   regexp = "input must contain 'name' and 'birthday' columns"
+ )
+
+ df <- data.frame(
+   birthday = c(as.Date("2001-01-01"), as.Date("2002-01-02"))
+ )
+ expect_error(
+   birthdays(df),
+   regexp = "input must contain 'name' and 'birthday' columns"
+ )
+
+
+})
